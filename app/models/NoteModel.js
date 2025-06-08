c718157550b2200ff53e5f62589b0e232b409055
YOUR_NAME <68414474+IHaveNoIdeaHowToCode@users.noreply.github.com>
2025-06-08 04:21:37 -0600

    Time working for Update and Create, color set as BG rn

--- app/models/NoteModel.js
+++ app/models/NoteModel.js
@@ -11,12 +11,16 @@ export class Note {
     this.title = data.title;
     this.color = data.color;
     this.body = data.body || 'Start jotting your thoughts here';
-    this.createdAt = data.createdAt ? new Date(data.createdAt) : new Date()
+    this.createdAt = data.createdAt == undefined ? new Date() : new Date(data.createdAt)
     this.updatedAt = data.updatedAt ? new Date(data.updatedAt) : new Date()
   }
 
-  get createdAtShortFormat() {
-    return this.createdAt.toLocaleDateString()
+  get createdAtDateFormat() {
+    return this.createdAt.toLocaleDateString('en-US', { month: 'long', day: '2-digit', year: 'numeric', })
+  }
+
+  get updatedAtDateFormat() {
+    return this.updatedAt.toLocaleDateString('en-US', { hour: 'numeric', minute: 'numeric', month: 'long', day: '2-digit', year: 'numeric', })
   }
 
   get ListTemplate() {
@@ -24,26 +28,28 @@ export class Note {
     <div role="button" onclick="app.NoteController.setActiveNote('${this.id}')" class="selectable-primary" title="Click to open Thought ${this.title}">
         <p class="fs-3 fw-bold">${this.title}</p>
         <p class="fs-5">${this.body}</p>
-        <p class="fs-7">${this.createdAt}</p>
-        <p class="fs-7">${this.updatedAt}</p>
+        <p class="fs-7">${this.createdAtDateFormat}</p>
+        <p class="fs-7">${this.updatedAtDateFormat}</p>
     </div>
     `
   }
 
   get activeNoteTemplate() {
     return `
-    <form id="active-form" class="row">
-      <h1>${this.title}</h1>
-      <div>${this.createdAt}</div>
-      <div>${this.updatedAt}</div>
+    <section style="background-color: ${this.color}; padding: 8px;">
+      <form id="active-form" class="row">
+        <h1>${this.title}</h1>
+        <div>${this.createdAtDateFormat}</div>
+        <div>${this.updatedAtDateFormat}</div>
 
-      <textarea name="body" class="col-12 form-control" rows="24">${this.body}</textarea>
+        <textarea name="body" class="col-12 form-control" rows="24">${this.body}</textarea>
 
-      <div class="d-flex justify-content-end my-2 gap-2">
+        <div class="d-flex justify-content-end my-2 gap-2">
           <button type="button" class="btn btn-danger">Delete</button>
           <button type="button"  class="btn btn-teal">Save</button>
         </div>
-    </form>
+      </form>
+    </section>
     `
   }
 }
